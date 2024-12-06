3f551d7b49d86cf45e157bf0bad48022a8961ca8
AleeZanatta <alejo.zanatta@davinci.edu.ar>
2024-12-05 21:11:14 -0300

    v3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './styles.css'; // Asegúrate de que la hoja de estilos esté incluida
 
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <div className="logo">
-                <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>
+            <div className="navbar-logo">
+                <Link to="/" className="navbar-title">
                     Museo
                 </Link>
             </div>
-            <ul>
+            <ul className="navbar-links">
                 <li>
-                    <Link to="/">Galería</Link>
+                    <Link to="/" className="navbar-link">Galería</Link>
                 </li>
                 <li>
-                    <Link to="/login">Iniciar Sesión</Link>
+                    <Link to="/login" className="navbar-link">Iniciar Sesión</Link>
                 </li>
                 <li>
-                    <Link to="/admin">Admin</Link>
+                    <Link to="/admin" className="navbar-link">Admin</Link>
                 </li>
             </ul>
         </nav>
