cbb384a877df56fd9ec0c1c8b93af7e51f360e37
AleeZanatta <alejo.zanatta@davinci.edu.ar>
2024-12-10 17:57:12 -0300

    nav bar bootstrap

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -1,27 +1,59 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaUser } from 'react-icons/fa';
-import './Navbar.css';
+import './Navbar.css'; // Opcional para agregar estilos personalizados
 
 const Navbar = () => {
-  const scrollToCollection = (id) => {
-    const element = document.getElementById(id);
-    if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
-
   return (
-    <nav className="navbar">
-      <div className="navbar-brand">
-        <Link to="/">Museum Name</Link>
-      </div>
-      <div className="navbar-auth">
-        <Link to="/login" className="auth-link">
-          <FaUser /> Login
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow">
+      <div className="container">
+        {/* Brand */}
+        <Link className="navbar-brand" to="/">
+          Museum Name
         </Link>
-        <Link to="/register" className="auth-button">
-          Register
-        </Link>
+        
+        {/* Toggler for responsive menu */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        
+        {/* Navbar Links */}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/collections">
+                Collections
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+            </li>
+          </ul>
+          
+          {/* Auth Links */}
+          <div className="d-flex ms-3">
+            <Link className="btn btn-outline-light me-2" to="/login">
+              Login
+            </Link>
+            <Link className="btn btn-primary" to="/register">
+              Register
+            </Link>
+          </div>
+        </div>
       </div>
     </nav>
   );
